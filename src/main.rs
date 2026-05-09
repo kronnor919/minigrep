f842ae4e3accf6bd2432e2e7f78a9b47c1c2e33e
kronnor919 <segundacuenta7364@gmail.com>
2026-05-09 18:54:15 -0400

    Improve output aesthetics and information
    - use `minigrep::handle_options`
    - pass only the two relevant arguments to `Config::build`
    - Code format

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 use std::env;
 use std::process;
 
+use minigrep::handle_options;
 use minigrep::{AppError, Config, run};
 
 fn main() {
+    // Read arguments
     let args: Vec<String> = env::args().collect();
 
-    let config = match Config::build(&args) {
-        Ok(r) => r,
+    // Extract options (--opt)
+    let options = &args.iter().filter(|arg| arg.starts_with("--")).collect();
+    handle_options(options);
+
+    // Build config                          Pass args without --
+    let config = match Config::build(&args[options.len() + 1..]) {
+        Ok(cfg) => cfg,
+
         Err(AppError::MissingArgs()) => {
             eprintln!("minigrep takes 2 arguments: [QUERY] [FILEPATH]");
+            eprintln!("Try 'minigrep --help' for more information");
             process::exit(1);
         }
 
         Err(e) => {
-            eprintln!("Error: {e}");
+            eprintln!("{e}");
             process::exit(1);
         }
     };
 
+    // Run search
     match run(config) {
         Ok(res) => {
+            println!("");
             for line in res {
                 println!("{line}");
             }
             process::exit(0);
         }
+
         Err(e) => {
             eprintln!("{e}");
             process::exit(1);
